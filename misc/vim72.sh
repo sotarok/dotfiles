17e3c0d0fcb1f34eba762363047213aad14e02d5
sotarok <sotarok@d0558523-a964-43cb-96e9-15da18736084>
2010-06-29 04:51:59 +0000

    0
    git-svn-id: http://svn.es.nequal.jp/public/dotfiles/sotarok@1690 d0558523-a964-43cb-96e9-15da18736084

--- misc/vim72.sh
+++ misc/vim72.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # download sources and extract
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.2.tar.bz2
-wget ftp://ftp.vim.org/pub/vim/extra/vim-7.2-lang.tar.gz
+test -f vim-7.2.tar.bz2 || wget ftp://ftp.vim.org/pub/vim/unix/vim-7.2.tar.bz2
+test -f vim-7.2-lang.tar.gz || wget ftp://ftp.vim.org/pub/vim/extra/vim-7.2-lang.tar.gz
 bzip2 -dc vim-7.2.tar.bz2 | tar xvf -
 tar zxvf vim-7.2-lang.tar.gz
 
 # download patches
 cd vim72/src
-wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.001-100.gz
-wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.101-200.gz
-gzip -d 7.2.001-100.gz
-gzip -d 7.2.101-200.gz
-wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.{201..267}
-
-patch -p3 < src/7.2.001-100
-patch -p3 < src/7.2.101-200
-for fn in src/7.2.2* ; do patch -p3 < $fn ; done
+#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.001-100.gz
+#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.101-200.gz
+#gzip -d 7.2.001-100.gz
+#gzip -d 7.2.101-200.gz
+#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.{201..267}
+#
+#patch -p3 < src/7.2.001-100
+#patch -p3 < src/7.2.101-200
+#for fn in src/7.2.2* ; do patch -p3 < $fn ; done
 
 # setup
 #cd ..
-./configure  --enable-multibyte --enable-gpm --enable-cscope --with-features=huge --enable-fontset --disable-gui --without-x --disable-xim --enable-pythoninterp --enable-perlinterp && make && make install
+./configure --prefix=$HOME --enable-multibyte --enable-gpm --enable-cscope --with-features=huge --enable-fontset --disable-gui --without-x --disable-xim --enable-pythoninterp --enable-perlinterp && make && make install && cd ../../ && rm -rf ./vim72 && rm vim*tar*
