c469f314244f172809b3b6915fbb1038806634b8
sotarok <sotarok@d0558523-a964-43cb-96e9-15da18736084>
2010-11-11 01:29:43 +0000

    fixed
    git-svn-id: http://svn.es.nequal.jp/public/dotfiles/sotarok@1721 d0558523-a964-43cb-96e9-15da18736084

--- misc/setup.sh
+++ misc/setup.sh
@@ -28,7 +28,8 @@ then
 fi
 
 cat $DOTFILES/ssh/authorized_keys | sort | uniq > $HOME/authorized_keys_merged
-if test -f $HOME/.ssh/authorized_keys && mv $HOME/.ssh/authorized_keys $HOME/.ssh/authorized_keys.orig
-mv $HOME/authorized_keys_merged $HOME/.ssh/authorized_keys
+test ! -d $HOME/.ssh && mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
+test -f $HOME/.ssh/authorized_keys && mv $HOME/.ssh/authorized_keys $HOME/.ssh/authorized_keys.orig
+mv $HOME/authorized_keys_merged $HOME/.ssh/authorized_keys && chmod 600 $HOME/.ssh/authorized_keys
 
 ln -sf $DOTFILES/bin/* $HOME/bin
