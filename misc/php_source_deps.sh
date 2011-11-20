ee0a11ea52b8c3af1dc5e32d410cab21f3621e50
Sotaro KARASAWA <sotarok@crocos.co.jp>
2011-11-21 02:40:22 +0900

    php source deps

---
+++ misc/php_source_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+sudo apt-get install build-essential bison re2c libxml2-dev zlib1g-dev \
+    libcurl4-openssl-dev libjpeg62-dev libpng12-dev libfreetype6-dev libt1-dev \
+    libmcrypt-dev libreadline-dev libtidy-dev libxslt1-dev libsqlite3-dev libbz2-dev \
+    libmysqlclient-dev libenchant-dev libgmp3-dev libc-client2007e-dev libicu-dev \
+    libsasl2-dev libonig-dev libpq-dev libpspell-dev libsnmp-dev
