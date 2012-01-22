1edf0fb5e9e4086d8c519f1e54d4c5682ba97bbb
Sotaro KARASAWA <sotarok@crocos.co.jp>
2012-01-23 03:54:24 +0900

    vim73

--- misc/vim73.sh
+++ misc/vim73.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+test -d vim73 && rm -rf vim73
+
 # download sources and extract
 test -f vim-7.3.tar.bz2 || wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
 #test -f vim-7.3-lang.tar.gz || wget ftp://ftp.vim.org/pub/vim/extra/vim-7.3-lang.tar.gz
@@ -8,18 +10,11 @@ tar xf vim-7.3.tar.bz2
 
 # download patches
 cd vim73
-#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.001-100.gz
-#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.101-200.gz
-#gzip -d 7.2.001-100.gz
-#gzip -d 7.2.101-200.gz
-#wget http://ftp.vim.org/pub/vim/patches/7.2/7.2.{201..267}
-#
-#patch -p3 < src/7.2.001-100
-#patch -p3 < src/7.2.101-200
-#for fn in src/7.2.2* ; do patch -p3 < $fn ; done
+
+ln -sf ../vim-patches
+cat vim-patches/7.3.* | patch -p0
 
 # setup
-#cd ..
 ./configure --prefix=$HOME --enable-multibyte --enable-gpm --enable-cscope --with-features=huge --enable-fontset --disable-gui --without-x --disable-xim --enable-pythoninterp --enable-perlinterk \
     && make \
     && make install \
