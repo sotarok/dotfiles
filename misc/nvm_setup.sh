3ec6bc9b8f2c3e25214c8ba9edde1ca9ad420320
Sotaro KARASAWA <sotarok@crocos.co.jp>
2012-04-02 16:53:16 +0900

    update

--- misc/nvm_setup.sh
+++ misc/nvm_setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+VERSION=$1
+echo $VERSION
+test -z "$VERSION" && exit
+
 cd $HOME
-git clone git://github.com/creationix/nvm.git ./.nvm
+if test ! -d ./.nvm
+then
+    git clone git://github.com/creationix/nvm.git ./.nvm
+    source ./.nvm/nvm.sh \
+        && nvm install v${VERSION} \
+        && nvm use $VERSION \
+        && nvm alias default $VERSION
+fi
+
+test ! -z "$(which node)" && curl http://npmjs.org/install.sh | sh
