03425b9b36507034e9774031ba2d20a8b800c535
Sotaro KARASAWA <sotarok@crocos.co.jp>
2012-04-05 11:20:40 +0900

    rvm setup

--- misc/rvm_setup.sh
+++ misc/rvm_setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+VERSION=$1
+echo $VERSION
+test -z "$VERSION" && exit
+
 cd $HOME
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+if test ! -d ./.rvm
+then
+    bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+    source "$HOME/.rvm/scripts/rvm" \
+        && rvm install ${VERSION} \
+        && rvm alias create default $VERSION \
+        && rvm use $VERSION
+fi
