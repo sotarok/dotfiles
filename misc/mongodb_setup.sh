57196fd54e314f4beb5126fe153acca75d122183
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-02-02 23:07:57 +0900

    update

--- misc/mongodb_setup.sh
+++ misc/mongodb_setup.sh
@@ -3,18 +3,8 @@
 
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-
-cat <<E >/tmp/mongo.list
-deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen
-E
-sudo mv /tmp/mongo.list /etc/apt/sources.list.d/mongo.list
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-org
 
-test -d /etc/munin \
-    && wget -O erh-mongo-munin.tar.gz http://github.com/erh/mongo-munin/tarball/master \
-    && tar xf erh-mongo-munin.tar.gz \
-    && sudo cp erh-mongo-munin*/mongo_* /etc/munin/plugins/ \
-    && sudo -u munin /usr/share/munin/munin-update \
-    && rm -rf erh-mongo-munin*
