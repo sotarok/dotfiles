3b616a80632ffda9078f01ec496442a4cd877759
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-07-07 12:26:03 +0900

    add mac_setup

---
+++ misc/mac_setup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -e
+
+usage() {
+    echo "USAGE: `basename $0` [Options]"
+    echo ""
+    echo "Required:"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help          show this help."
+    echo "  -v, --verbose       show detail commands."
+    echo "  --dry               dry run mode."
+    exit 1;
+}
+
+main() {
+    script_dir=$(cd $(dirname $0); pwd)
+    opts=`getopt -o hvo: -l help,verbose,option:,dry, -- "$@"`
+    eval set -- "$opts"
+    while [ -n "$1" ]; do
+        case $1 in
+            -h|--help) usage;;
+            -o|--option) option=$2; shift;;
+            -v|--verbose) is_verbose=1;;
+            --dry) is_dry=1;;
+            --) shift; break;;
+            *) usage;;
+        esac
+        shift
+    done
+
+    if [ $is_dry ];then
+        info "dry run..."
+    fi
+
+    run ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    run brew install caskroom/cask/brew-cask
+    run brew cask install macdown
+
+}
+
+## utility
+run() {
+    if [ $is_dry ]; then
+        echo "[dry run] $@"
+    else
+        if [ $is_verbose ];then
+            echo "[run] $@"
+        fi
+        eval "$@"
+    fi
+}
+
+red() {
+    echo -n "[1;31m$1[0m"
+}
+
+yellow() {
+    echo -n "[1;33m$1[0m"
+}
+
+green() {
+    echo -n "[1;32m$1[0m"
+}
+
+fatal() {
+    red "[fatal] "
+    echo "$1"
+}
+
+warn() {
+    yellow "[warn] "
+    echo "$1"
+}
+
+info() {
+    green "[info] "
+    echo "$1"
+}
+
+# call main.
+main "$@"
+
