a1c8a84e8362a8985f1eb7ce20ab09de018f96ac
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-08-12 14:13:37 +0900

    update

--- misc/mac_setup.sh
+++ misc/mac_setup.sh
@@ -35,11 +35,7 @@ main() {
 
     run ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     run brew install caskroom/cask/brew-cask
-    run brew install vim
-    run brew install tree
-    run brew install mobile-shell
-    run brew install tmux
-    run brew install the_silver_searcher
+    run brew install vim tree mobile-shell tmux the_silver_searcher gnu-sed
     run brew cask install macdown
 
 }
