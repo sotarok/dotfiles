ba1e82d319d109427768dbd1e858ebda17759a85
Sotaro KARASAWA <sotaro.k@gmail.com>
2015-11-10 20:13:19 +0900

    update

--- ansible/jessie-dev/boostrap.sh
+++ ansible/jessie-dev/boostrap.sh
@@ -3,5 +3,6 @@
 sudo apt-get -y update
 sudo apt-get install -y software-properties-common curl vim python-pip python-dev build-essential python-apt
 sudo pip install ansible markupsafe
+ansible-galaxy install -r requirements.yml -p roles/
 
 ansible-playbook -i "localhost," -c local playbook.yml
